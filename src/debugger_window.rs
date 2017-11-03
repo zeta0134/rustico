1ccaadca45f867bf1fa7fb9ca9ce19a222812fda
Nicholas Flynt <thegamefreak0134@gmail.com>
2017-11-03 01:46:36 -0500

    Add disassembly to the debugger window
    Most of the actual changes to support this are upstream in rusticnes-core. I'm not super happy with the layout, but it's functional.

--- src/debugger_window.rs
+++ src/debugger_window.rs
@@ -1,6 +1,8 @@
 extern crate sdl2;
 
 use rusticnes_core::nes::NesState;
+use rusticnes_core::opcode_info::disassemble_instruction;
+use rusticnes_core::memory;
 
 use sdl2::event::Event;
 use sdl2::event::WindowEvent;
@@ -24,7 +26,7 @@ impl DebuggerWindow {
   pub fn new(sdl_context: &sdl2::Sdl) -> DebuggerWindow {
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("Debugger", 400, 600)
+    let window = video_subsystem.window("Debugger", 512, 600)
         .position(570, 50)
         .hidden()
         .opengl()
@@ -40,7 +42,7 @@ impl DebuggerWindow {
 
     return DebuggerWindow {
       canvas: canvas,
-      buffer: SimpleBuffer::new(200, 300),
+      buffer: SimpleBuffer::new(256, 300),
       font: font,
       shown: false,
     }
@@ -48,7 +50,7 @@ impl DebuggerWindow {
 
   pub fn draw_registers(&mut self, nes: &mut NesState, x: u32, y: u32) {
     drawing::text(&mut self.buffer, &self.font, x, y, 
-      "==== Registers ====", 
+      "===== Registers =====", 
       &[192, 192, 192, 255]);
     drawing::text(&mut self.buffer, &self.font, x, y + 8, 
       &format!("A: 0x{:02X}", nes.registers.a), &[255, 255, 128, 255]);
@@ -76,12 +78,38 @@ impl DebuggerWindow {
       &[128, 192, 128, 255]);
   }
 
+  pub fn draw_disassembly(&mut self, nes: &mut NesState, x: u32, y: u32) {
+    drawing::text(&mut self.buffer, &self.font, x, y, 
+    "===== Disassembly =====", &[255, 255, 255, 255]);
+
+    let mut data_bytes_to_skip = 0;
+    for i in 0 .. 30 {
+      let pc = nes.registers.pc + (i as u16);
+      let opcode = memory::passively_read_byte(nes, pc);
+      let data1 = memory::passively_read_byte(nes, pc + 1);
+      let data2 = memory::passively_read_byte(nes, pc + 2);
+      let (instruction, data_bytes) = disassemble_instruction(opcode, data1, data2);
+      let mut text_color = [255, 255, 255, 255];
+      if data_bytes_to_skip > 0 {
+        text_color = [64, 64, 64, 255];
+        data_bytes_to_skip -= 1;
+      } else {
+        data_bytes_to_skip = data_bytes;
+      }
+
+      drawing::text(&mut self.buffer, &self.font, x, y + 16 + (i as u32 * 8),
+        &format!("0x{:04X} - 0x{:02X}:  {}", pc, opcode, instruction),
+        &text_color);     
+    }
+  }
+
   pub fn update(&mut self, nes: &mut NesState) {
     // Clear!
     let width = self.buffer.width;
     let height = self.buffer.height;
     drawing::rect(&mut self.buffer, 0, 0, width, height, &[0,0,0,255]);
     self.draw_registers(nes, 0, 0);
+    self.draw_disassembly(nes, 0, 40);
   }
 
   pub fn draw(&mut self) {
