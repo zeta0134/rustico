3dfda3fcbf8bd44f172f431ab0ebd49deb447723
Nicholas Flynt <thegamefreak0134@gmail.com>
2017-11-13 01:57:43 -0600

    Rewrite vram drawing code for performance
    This uses one large SimpleBuffer instead of a handful of smaller ones. More importantly, it moves the tile drawing code into its own function, and critically, only reads the memory for a particular tile once per tile. Before it was doing this once per *pixel* and effectively doing like 126 extra function calls per tile when drawing the nametable.
    
    Now, on my Windows machine at least, I can run a game with the VRAM window open and finally have no slowdown. The game continues to run at 60 FPS even with all the debug code running every frame.

--- src/vram_window.rs
+++ src/vram_window.rs
@@ -14,19 +14,53 @@ use sdl2::render::TextureAccess;
 
 use drawing::SimpleBuffer;
 
+pub fn draw_tile(mapper: &mut Mapper, pattern_address: u16, tile_index: u16, buffer: &mut SimpleBuffer, dx: u32, dy: u32, palette: &[u8]) {
+  for py in 0 .. 8 {
+    let tile_address = pattern_address + tile_index * 16 + py;
+    let mut tile_low  = mapper.debug_read_byte(tile_address);
+    let mut tile_high = mapper.debug_read_byte(tile_address + 8);
+    for px in 0 .. 8 {
+      let palette_index = (tile_low & 0x1) + ((tile_high & 0x1) << 1);
+      tile_low = tile_low >> 1;
+      tile_high = tile_high >> 1;
+      buffer.put_pixel(
+        dx + (7 - px as u32), 
+        dy + (py as u32), &[
+          palette[(palette_index * 4 + 0) as usize],
+          palette[(palette_index * 4 + 1) as usize],
+          palette[(palette_index * 4 + 2) as usize],
+          255]);
+    }
+  }
+}
+
+pub fn generate_chr_pattern(mapper: &mut Mapper, pattern_address: u16, buffer: &mut SimpleBuffer, dx: u32, dy: u32) {
+  let debug_palette: [u8; 4*4] = [
+    255, 255, 255, 255,
+    192, 192, 192, 255,
+    128, 128, 128, 255,
+      0,   0,   0, 255];
+  for x in 0 .. 16 {
+    for y in 0 .. 16 {
+      let tile_index = y * 16 + x;
+      draw_tile(mapper, pattern_address, tile_index as u16, buffer, 
+        dx + x * 8, dy + y * 8, &debug_palette);
+    }
+  }
+}
+
 pub struct VramWindow {
   pub canvas: sdl2::render::WindowCanvas,
-  pub chr0_buffer: SimpleBuffer,
-  pub chr1_buffer: SimpleBuffer,
-  pub nametable_buffer: SimpleBuffer,
+  pub buffer: SimpleBuffer,
   pub shown: bool,
+  pub palette_cache: [[u8; 4*4]; 4*2],
 }
 
 impl VramWindow {
   pub fn new(sdl_context: &sdl2::Sdl) -> VramWindow {
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("VRAM Debugger", 512, 736)
+    let window = video_subsystem.window("VRAM Debugger", 768, 512)
         .position(570, 50)
         .hidden()
         .opengl()
@@ -40,79 +74,64 @@ impl VramWindow {
 
     return VramWindow {
       canvas: canvas,
-      chr0_buffer: SimpleBuffer::new(128, 128),
-      chr1_buffer: SimpleBuffer::new(128, 128),
-      nametable_buffer: SimpleBuffer::new(512, 480),
+      buffer: SimpleBuffer::new(768, 512),
       shown: false,
+      palette_cache: [[0u8; 4*4]; 4*2]
     }
   }
 
-  pub fn generate_chr_pattern(mapper: &mut Mapper, pattern_address: u16, buffer: &mut SimpleBuffer) {
-    let debug_pallete: [u8; 4] = [255, 192, 128, 0];
-    for x in 0 .. 16 {
-      for y in 0 .. 16 {
-        let tile = y * 16 + x;
-        for px in 0 .. 8 {
-          for py in 0 .. 8 {
-            let palette_index = ppu::decode_chr_pixel(mapper, pattern_address, tile as u8, px as u8, py as u8);
-            buffer.put_pixel(x * 8 + px, y * 8 + py, &[
-              debug_pallete[palette_index as usize],
-              debug_pallete[palette_index as usize],
-              debug_pallete[palette_index as usize],
-              255]);
-          }
-        }
+  pub fn update_palette_cache(&mut self, nes: &mut NesState) {
+    // Initialize all palette colors with a straight copy
+    for p in 0 .. 8 {
+      for i in 0 .. 4 {
+        let palette_color = nes.ppu.passively_read_byte(&mut *nes.mapper, 0x3F00 + p * 4 + i) as usize * 3;
+        self.palette_cache[p as usize][i as usize * 4 + 0] = NTSC_PAL[palette_color + 0];
+        self.palette_cache[p as usize][i as usize * 4 + 1] = NTSC_PAL[palette_color + 1];
+        self.palette_cache[p as usize][i as usize * 4 + 2] = NTSC_PAL[palette_color + 2];
+        self.palette_cache[p as usize][i as usize * 4 + 3] = 255;
       }
     }
+
+    // Override the background colors with the universal background color:
+    for p in 1 .. 8 {
+      self.palette_cache[p][0] = self.palette_cache[0][0];
+      self.palette_cache[p][1] = self.palette_cache[0][1];
+      self.palette_cache[p][2] = self.palette_cache[0][2];
+      self.palette_cache[p][3] = 255;
+    }
   }
 
-  pub fn generate_nametables(mapper: &mut Mapper, ppu: &mut ppu::PpuState, buffer: &mut SimpleBuffer) {
+  pub fn generate_nametables(&mut self, mapper: &mut Mapper, ppu: &mut ppu::PpuState, dx: u32, dy: u32) {
     let mut pattern_address = 0x0000;
     if (ppu.control & 0x10) != 0 {
       pattern_address = 0x1000;
     }
+    
     for tx in 0 .. 64 {
       for ty in 0 .. 60 {
         let tile_index = ppu.get_bg_tile(mapper, tx, ty);
         let palette_index = ppu.get_bg_palette(mapper, tx, ty);
-        for px in 0 .. 8 {
-          for py in 0 .. 8 {
-            let bg_index = ppu::decode_chr_pixel(mapper, pattern_address, tile_index as u8, px as u8, py as u8);
-            let mut palette_color = ppu.passively_read_byte(mapper, ((palette_index << 2) + bg_index) as u16 + 0x3F00) as usize * 3;
-            if bg_index == 0 {
-              palette_color = ppu.passively_read_byte(mapper, bg_index as u16 + 0x3F00) as usize * 3;
-            }
-            buffer.put_pixel(tx as u32 * 8 + px as u32, ty as u32 * 8 + py as u32, &[
-              NTSC_PAL[palette_color + 0],
-              NTSC_PAL[palette_color + 1],
-              NTSC_PAL[palette_color + 2],
-              255]);
-          }
-        }
+        draw_tile(mapper, pattern_address, tile_index as u16, &mut self.buffer, 
+          dx + tx as u32 * 8, dy + ty as u32 * 8, &self.palette_cache[palette_index as usize]);
       }
     }
   }
 
   pub fn update(&mut self, nes: &mut NesState) {
-    VramWindow::generate_nametables(&mut *nes.mapper, &mut nes.ppu, &mut self.nametable_buffer);
-    VramWindow::generate_chr_pattern(&mut *nes.mapper, 0x0000, &mut self.chr0_buffer);
-    VramWindow::generate_chr_pattern(&mut *nes.mapper, 0x1000, &mut self.chr1_buffer);
+    self.update_palette_cache(nes);
+    generate_chr_pattern(&mut *nes.mapper, 0x0000, &mut self.buffer,   0, 0);
+    generate_chr_pattern(&mut *nes.mapper, 0x1000, &mut self.buffer, 128, 0);
+    self.generate_nametables(&mut *nes.mapper, &mut nes.ppu, 256, 0);
   }
   
 
   pub fn draw(&mut self) {
     let texture_creator = self.canvas.texture_creator();
-    let mut nametable_texture = texture_creator.create_texture(PixelFormatEnum::ABGR8888, TextureAccess::Streaming, 512, 480).unwrap();
-    let mut chr_0_texture = texture_creator.create_texture(PixelFormatEnum::ABGR8888, TextureAccess::Streaming, 128, 128).unwrap();
-    let mut chr_1_texture = texture_creator.create_texture(PixelFormatEnum::ABGR8888, TextureAccess::Streaming, 128, 128).unwrap();
+    let mut texture = texture_creator.create_texture(PixelFormatEnum::ABGR8888, TextureAccess::Streaming, self.buffer.width, self.buffer.height).unwrap();
       
     self.canvas.set_draw_color(Color::RGB(255, 255, 255));
-    let _ = nametable_texture.update(None, &self.nametable_buffer.buffer, 512 * 4);
-    let _ = chr_0_texture.update(None, &self.chr0_buffer.buffer, 128 * 4);
-    let _ = chr_1_texture.update(None, &self.chr1_buffer.buffer, 128 * 4);
-    let _ = self.canvas.copy(&chr_0_texture    , None, Rect::new(  0,   0, 256, 256));
-    let _ = self.canvas.copy(&chr_1_texture    , None, Rect::new(256,   0, 256, 256));
-    let _ = self.canvas.copy(&nametable_texture, None, Rect::new(  0, 256, 512, 480));
+    let _ = texture.update(None, &self.buffer.buffer, (self.buffer.width * 4) as usize);
+    let _ = self.canvas.copy(&texture, None, None);
 
     self.canvas.present();
   }
