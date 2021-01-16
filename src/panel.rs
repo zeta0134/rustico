aea1cd6b8c3c2f5cf13b76112cf7aed8db9b1813
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-01-16 07:04:36 -0500

    Use Vec<Event> instead of a slice. Also, use implicit &self lifetime

--- src/panel.rs
+++ src/panel.rs
@@ -2,7 +2,7 @@ use events::Event;
 use drawing::SimpleBuffer;
 
 pub trait Panel {
-    fn title(&self) -> String;
-    fn handle_event(Event) -> [Event];
-    fn active_canvas<'a>(&'a self) -> &'a SimpleBuffer;
+    fn title(&self) -> &str;
+    fn handle_event(&mut self, event: Event) -> Vec<Event>;
+    fn active_canvas(&self) -> &SimpleBuffer;
 }
