b354e5bf20e34ac1b9cead92e499d5723441810d
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-01-17 12:40:39 -0500

    Implement RunState events for muting the 5 stock audio channels

--- src/application.rs
+++ src/application.rs
@@ -1,3 +1,5 @@
+use events::Event;
+
 use rusticnes_core::nes::NesState;
 use rusticnes_core::mmc::none::NoneMapper;
 
@@ -10,5 +12,27 @@ impl RuntimeState {
         return RuntimeState {
             nes: NesState::new(Box::new(NoneMapper::new())),
         }
+    }
+
+    pub fn handle_event(&mut self, event: Event) -> Vec<Event> {
+        match event {
+            Event::ApuTogglePulse1 => {
+                self.nes.apu.pulse_1.debug_disable = !self.nes.apu.pulse_1.debug_disable;
+            },
+            Event::ApuTogglePulse2 => {
+                self.nes.apu.pulse_2.debug_disable = !self.nes.apu.pulse_2.debug_disable;
+            },
+            Event::ApuToggleTriangle => {
+                self.nes.apu.triangle.debug_disable = !self.nes.apu.triangle.debug_disable;
+            },
+            Event::ApuToggleNoise => {
+                self.nes.apu.noise.debug_disable = !self.nes.apu.noise.debug_disable;
+            },
+            Event::ApuToggleDmc => {
+                self.nes.apu.dmc.debug_disable = !self.nes.apu.dmc.debug_disable;
+            },
+            _ => {}
+        }
+        return Vec::<Event>::new();
     }
 }
--- src/events.rs
+++ src/events.rs
@@ -1,6 +1,11 @@
 #[derive(Copy, Clone)]
 pub enum Event {
     CloseWindow,
+    ApuTogglePulse1,
+    ApuTogglePulse2,
+    ApuToggleTriangle,
+    ApuToggleNoise,
+    ApuToggleDmc,
     RequestFrame,
     ShowCpuWindow,
     ShowPpuWindow,
