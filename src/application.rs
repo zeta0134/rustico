080c174640ccb06f4b5d08c47dccb0928466a45d
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-01-24 17:33:43 -0500

    Expand Cartridge and Sram management events
    In particular, we now allow these events to pass a string id, which
    the application state currently does not use, but platform code
    can use to keep track of responses without needing to manually
    guess state.

--- src/application.rs
+++ src/application.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use events::Event;
 
 use rusticnes_core::nes::NesState;
@@ -18,27 +20,31 @@ impl RuntimeState {
         }
     }
 
-    pub fn load_cartridge(&mut self, file_data: &[u8]) {
+    pub fn load_cartridge(&mut self, cart_id: String, file_data: &[u8]) -> Event {
         let maybe_nes = NesState::from_rom(&file_data);
         match maybe_nes {
             Ok(nes_state) => {
                 self.nes = nes_state;
                 self.running = true;
                 self.file_loaded = true;
+                return Event::CartridgeLoaded(cart_id);
             },
             Err(why) => {
-                println!("{}", why);
+                return Event::CartridgeRejected(cart_id, why);
             }
         }
     }
 
     pub fn load_sram(&mut self, file_data: &[u8]) {
         if self.nes.mapper.has_sram() {
-            self.nes.set_sram(file_data.to_vec());
+            if file_data.len() > 0 {
+                self.nes.set_sram(file_data.to_vec());
+            }
         }
     }
 
     pub fn handle_event(&mut self, event: Event) -> Vec<Event> {
+        let mut responses: Vec<Event> = Vec::new();
         match event {
             Event::ApuTogglePulse1 => {
                 self.nes.apu.pulse_1.debug_disable = !self.nes.apu.pulse_1.debug_disable;
@@ -55,17 +61,23 @@ impl RuntimeState {
             Event::ApuToggleDmc => {
                 self.nes.apu.dmc.debug_disable = !self.nes.apu.dmc.debug_disable;
             },
-            Event::LoadCartridge(file_data) => {
-                self.load_cartridge(&file_data);
+            Event::LoadCartridge(cart_id, file_data, sram_data) => {
+                responses.push(self.load_cartridge(cart_id, &file_data));
+                self.load_sram(&sram_data);
             },
             Event::LoadSram(sram_data) => {
                 self.load_sram(&sram_data);
             },
             Event::NesRunFrame => {
                 self.nes.run_until_vblank();
+            },
+            Event::RequestSramSave(sram_id) => {
+                if self.nes.mapper.has_sram()  {
+                    responses.push(Event::SaveSram(sram_id, Rc::new(self.nes.sram())));
+                }
             }
             _ => {}
         }
-        return Vec::<Event>::new();
+        return responses;
     }
 }
