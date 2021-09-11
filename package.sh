7e0fd389cefe3d8c3359718044e65b30837c5636
zeta0134 <zeta0134@users.noreply.github.com>
2021-09-11 13:47:44 -0400

    Create zip files with .zip extensions. (whoops)

--- package.sh
+++ package.sh
@@ -19,8 +19,8 @@ echo "=== Windows - x86_64 ==="
 cargo build --release --target x86_64-pc-windows-gnu
 cd target/x86_64-pc-windows-gnu/release
 cp ../../../SDL2.dll .
-zip rusticnes-sdl-${VERSION}.Windows.x86_64.tar.gz rusticnes-sdl.exe SDL2.dll
+zip rusticnes-sdl-${VERSION}.Windows.x86_64.zip rusticnes-sdl.exe SDL2.dll
 cd ../../..
-mv target/x86_64-pc-windows-gnu/release/rusticnes-sdl-${VERSION}.Windows.x86_64.tar.gz packages
+mv target/x86_64-pc-windows-gnu/release/rusticnes-sdl-${VERSION}.Windows.x86_64.zip packages
 
 echo "Complete!"
