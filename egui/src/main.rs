4c14278b0d757a2e1d28d9ca6cdd3ed3bba6d304
Zeta <zeta0134@reploid.cafe>
2024-03-20 08:58:45 -0400

    Get RusticNES hooked up to the egui build and displaying frames
    It... works? It works! That was way simpler than expected. Yay!

--- egui/src/main.rs
+++ egui/src/main.rs
@@ -2,47 +2,55 @@
 
 use eframe::egui;
 
-fn silly_paint(canvas: &mut [u8], width: usize, height: usize, frame_counter: u8) {
-    for x in 0 .. width {
-        for y in 0 .. height {
-            canvas[y*256*4 + x*4 + 0] = (x % 256) as u8;
-            canvas[y*256*4 + x*4 + 1] = (y % 256) as u8;
-            canvas[y*256*4 + x*4 + 2] = x as u8 ^ y as u8 ^ frame_counter;
-            canvas[y*256*4 + x*4 + 3] = 255;
-        }
-    }
-}
+extern crate rusticnes_core;
+extern crate rusticnes_ui_common;
+
+use rusticnes_ui_common::application::RuntimeState as RusticNesRuntimeState;
+use rusticnes_ui_common::events;
+use rusticnes_ui_common::game_window::GameWindow;
+use rusticnes_ui_common::panel::Panel;
 
 struct RusticNesGameWindow {
-    pub canvas: [u8; 256*240*4],
-    pub texture_handle: egui::TextureHandle,
-    pub frame_counter: u8,
+    pub texture_handle: egui::TextureHandle,    
+    pub runtime_state: RusticNesRuntimeState,
+    pub game_window: GameWindow
 }
 
 impl RusticNesGameWindow {
     fn new(cc: &eframe::CreationContext) -> Self {
-        let mut game_canvas = [0u8; 256*240*4];
-        silly_paint(&mut game_canvas, 256, 240, 0);
-        let image = egui::ColorImage::from_rgba_unmultiplied([256,240], &game_canvas);
+        let game_window = GameWindow::new();
+        let image = egui::ColorImage::from_rgba_unmultiplied([256,240], &game_window.canvas.buffer);
         let texture_handle = cc.egui_ctx.load_texture("game_window_canvas", image, egui::TextureOptions::default());
 
+        let mut runtime_state = RusticNesRuntimeState::new();
+
+        let cartridge_data = std::fs::read("cartridge.nes").unwrap();
+        let bucket_of_nothing: Vec<u8> = Vec::new();
+        let cartridge_load_event = rusticnes_ui_common::Event::LoadCartridge(
+            "cartridge.nes".to_string(), std::rc::Rc::new(cartridge_data), std::rc::Rc::new(bucket_of_nothing));
+        runtime_state.handle_event(cartridge_load_event);
+
         Self {
-            canvas: game_canvas,
+            game_window: game_window,
             texture_handle: texture_handle,
-            frame_counter: 0,
+            runtime_state: runtime_state,
         }   
     }
 }
 
 impl eframe::App for RusticNesGameWindow {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        silly_paint(&mut self.canvas, 256, 240, self.frame_counter);
-        let image = egui::ColorImage::from_rgba_unmultiplied([256,240], &self.canvas);
-        self.texture_handle.set(image, egui::TextureOptions::default());
-        self.frame_counter = self.frame_counter.wrapping_add(1);
-        ctx.request_repaint();
-
         // Presumably this is called at some FPS? I guess we can find out!
+
+        // For now, just run a frame and then draw it.
+        // TODO: once we have working audio, sync to that properly. This
+        // will run at completely the wrong speed if the host FPS isn't 60 Hz,
+        // or we lag, or don't vsync, etc etc
+        self.runtime_state.handle_event(events::Event::NesRunFrame);
+        self.game_window.handle_event(&self.runtime_state, events::Event::RequestFrame);
+        let image = egui::ColorImage::from_rgba_unmultiplied([256,240], &self.game_window.canvas.buffer);
+        self.texture_handle.set(image, egui::TextureOptions::default());
+
         egui::TopBottomPanel::top("game_window_top_panel").show(ctx, |ui| {
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("File", |ui| {
@@ -61,6 +69,8 @@ impl eframe::App for RusticNesGameWindow {
             ui.heading("Simple Canvas Painting");
             ui.image(egui::load::SizedTexture::from_handle(&self.texture_handle));
         });
+
+        ctx.request_repaint();
     }
 }
 
